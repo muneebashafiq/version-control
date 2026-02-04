64c5470f3505923ae8f71d9fb03df6e523c4d159
MUNEEBA <muneebaswatii@gmail.com>
2026-02-04 21:34:13 +0500

    second commit

--- 1.js
+++ 1.js
@@ -1,31 +1,31 @@
-// // var a = 9;
+// var a = 9;
 
-// function app(){
-//     let a = 10
-//     var b = 10
-//     console.log("value of a as initial ",a)
-//     console.log("value of b as initial ",b)
+function app(){
+    let a = 10
+    var b = 10
+    console.log("value of a as initial ",a)
+    console.log("value of b as initial ",b)
 
-//     // let a = 30
-//     // var b =20
-//     console.log(b)
-//     // console.log(a)
-//     // if(a>0){
-//     //     console.log("Positive number")
-//     // }
+    // let a = 30
+    // var b =20
+    console.log(b)
+    // console.log(a)
+    // if(a>0){
+    //     console.log("Positive number")
+    // }
 
-//     if(a>0){
-//         let a = 30;
-//         var b = 20
-//         console.log("value of a inside if block:", a)
-//         console.log("value of b inside if block:", b)
-//     }
-//     console.log("value of a outside if block:", a)
-//     console.log("value of b outside if block:", b)
-// }
+    if(a>0){
+        let a = 30;
+        var b = 20
+        console.log("value of a inside if block:", a)
+        console.log("value of b inside if block:", b)
+    }
+    console.log("value of a outside if block:", a)
+    console.log("value of b outside if block:", b)
+}
 
-// // console.log(a)
-// app()
+// console.log(a)
+app()
 
 declarationfunction()
 function declarationfunction(){
